cadc67683e84e9156f58db3dfbf62d82df37ee50
Rafay Hassan <rafay.hassan@outlook.com>
2020-08-18 11:44:27 +0500

    refac: date trasnformation

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,12 +3,12 @@ const bcrypt = require("bcryptjs");
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const Booking = require("../../models/booking");
-
+const { dateToString } = require("../../helpers/date");
 const transformEvent = (event) => {
   return {
     ...event._doc,
     _id: event.id,
-    date: new Date(event._doc.date).toISOString(),
+    date: dateToString(event._doc.date),
     creator: user.bind(this, event.creator),
   };
 };
@@ -107,8 +107,8 @@ module.exports = {
           _id: booking.id,
           user: user.bind(this, booking._doc.user),
           event: singleEvent.bind(this, booking._doc.event),
-          createdAt: new Date(booking._doc.createdAt).toISOString(),
-          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
+          createdAt: dateToString(booking._doc.createdAt),
+          updatedAt: dateToString(booking._doc.updatedAt),
         };
       });
     } catch (err) {
@@ -128,8 +128,8 @@ module.exports = {
       _id: result.id,
       user: user.bind(this, result._doc.user),
       event: singleEvent.bind(this, result._doc.event),
-      createdAt: new Date(result._doc.createdAt).toISOString(),
-      updatedAt: new Date(result._doc.updatedAt).toISOString(),
+      createdAt: dateToString(result._doc.createdAt),
+      updatedAt: dateToString(result._doc.updatedAt),
     };
   },
   cancelBooking: async (args) => {
