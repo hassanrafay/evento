0622766a0b899a7a148046f55ba8e88157946445
Rafay Hassan <rafay.hassan@outlook.com>
2020-08-18 11:49:21 +0500

    refac: booking obj transformation

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,12 +4,24 @@ const Event = require("../../models/event");
 const User = require("../../models/user");
 const Booking = require("../../models/booking");
 const { dateToString } = require("../../helpers/date");
+
 const transformEvent = (event) => {
   return {
     ...event._doc,
     _id: event.id,
     date: dateToString(event._doc.date),
     creator: user.bind(this, event.creator),
+  };
+};
+
+const transformBooking = (booking) => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt),
   };
 };
 
@@ -102,14 +114,7 @@ module.exports = {
     try {
       const bookings = await Booking.find();
       return bookings.map((booking) => {
-        return {
-          ...booking._doc,
-          _id: booking.id,
-          user: user.bind(this, booking._doc.user),
-          event: singleEvent.bind(this, booking._doc.event),
-          createdAt: dateToString(booking._doc.createdAt),
-          updatedAt: dateToString(booking._doc.updatedAt),
-        };
+        return transformBooking(booking);
       });
     } catch (err) {
       throw err;
@@ -123,14 +128,7 @@ module.exports = {
       event: fetchedEvent.id,
     });
     const result = await booking.save();
-    return {
-      ...result._doc,
-      _id: result.id,
-      user: user.bind(this, result._doc.user),
-      event: singleEvent.bind(this, result._doc.event),
-      createdAt: dateToString(result._doc.createdAt),
-      updatedAt: dateToString(result._doc.updatedAt),
-    };
+    return transformBooking(result);
   },
   cancelBooking: async (args) => {
     try {
