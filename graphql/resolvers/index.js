82797578264338cef1d341a649215ddc712944c2
Rafay Hassan <rafay.hassan@outlook.com>
2020-08-18 06:20:28 +0500

    formated event date

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -24,6 +24,7 @@ const events = (eventIds) => {
         return {
           ...event._doc,
           _id: event.id,
+          date: new Date(event._doc.date).toISOString(),
           creator: user.bind(this, event.creator),
         };
       });
@@ -41,6 +42,7 @@ module.exports = {
           return {
             ...event._doc,
             _id: event.id,
+            date: new Date(event._doc.date).toISOString(),
             creator: user.bind(this, event._doc.creator),
           };
         });
@@ -64,6 +66,7 @@ module.exports = {
         createdEvent = {
           ...result._doc,
           _id: result.id,
+          date: new Date(event._doc.date).toISOString(),
           creator: user.bind(this, result._doc.creator),
         };
         return User.findById("5f39f741c7aae10fe92a8b39");
