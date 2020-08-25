3916c8696edf9a9b7abcc111bac7edce32914f82
Rafay Hassan <rafay.hassan@outlook.com>
2020-08-25 13:21:36 +0500

    improved performance

--- graphql/resolvers/helper.js
+++ graphql/resolvers/helper.js
@@ -1,15 +1,24 @@
+const DataLoader = require("dataloader");
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const { dateToString } = require("../../helpers/date");
 
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+  return User.find({ _id: { $in: userIds } });
+});
+
 const user = async (userId) => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
       password: null,
-      createdEvents: events.bind(this, user._doc.createdEvents),
+      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents),
     };
   } catch (err) {
     throw err;
@@ -29,8 +38,8 @@ const events = async (eventIds) => {
 
 const singleEvent = async (eventId) => {
   try {
-    const event = await Event.findById({ _id: eventId });
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (err) {
     throw err;
   }
